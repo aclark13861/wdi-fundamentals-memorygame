fab3bf7a2499fb29709e2f9a16da8247c47d3ac1
Andrew Clark <andrewscomputer@Andrews-MacBook-Air.local>
2020-01-07 16:14:23 -0700

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,17 @@
+console.log("Up and running!");
+
+let cards = ["Queen","King","Queen","King"]
+
+let cardsInPlay = []
+let cardOne = "Queen"
+	cardsInPlay.push('cardOne')
+		console.log("User Flipped Queen");
+let cardTwo = "King"
+	cardsInPlay.push('cardTwo')
+		console.log("User Flipped King")
+
+if (cardsInPlay.length === 2 + cardsInPlay[0] === cardsInPlay[1])
+		alert("You found a match!")
+	else {
+		alert("Sorry, try again");
+	};
