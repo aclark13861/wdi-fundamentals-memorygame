3e21a43e0072dd0eca766120058fc796718a51f5
Andrew Clark <andrewscomputer@Andrews-MacBook-Air.local>
2020-01-11 13:47:28 -0700

    created logic re try

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,17 +1,56 @@
 console.log("Up and running!");
 
-let cards = ["Queen","King","Queen","King"]
+const cards = [{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png",
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png",
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png",
 
-let cardsInPlay = []
-let cardOne = "Queen"
-	cardsInPlay.push('cardOne')
-		console.log("User Flipped Queen");
-let cardTwo = "King"
-	cardsInPlay.push('cardTwo')
-		console.log("User Flipped King")
+}
+];
 
-if (cardsInPlay.length === 2 + cardsInPlay[0] === cardsInPlay[1])
+
+let cardId;
+cards1 = ["queen","queen","king","king"];
+let cardsInPlay = [];
+var cardOne = cards1[0];
+var cardTwo = cards1[2];
+
+cardsInPlay.push('cardOne');
+	console.log("User flipped queen");
+cardsInPlay.push('cardTwo');
+	console.log("User flipped king");
+
+	if (cardsInPlay[0] === cardsInPlay[1])
 		alert("You found a match!")
 	else {
 		alert("Sorry, try again");
+		};
+	
+
+var flipCard = function(cardId){
+	flipCard(0);
+	flipCard(2);
+
+	console.log("User Flipped " + cards[cardId].rank);
+
 	};
+			
+	
+
+
+		
